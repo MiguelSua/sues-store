32353fce862afdc79e2339814d3f29c9b85102ff
SUES <miangelse45@gmail.com>
2025-07-18 18:31:04 -0500

    🐛 Corrijo ruta del endpoint /pedido

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './index.css';
 import App from './App';
-import CancelarCita from './CancelarCita';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
 root.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/cancelar/:token" element={<CancelarCita />} />
-    </Routes>
-  </BrowserRouter>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
