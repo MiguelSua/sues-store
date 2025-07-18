eca1bce25ccb733fdf7a77e4b78c8e58d5776f2d
SUES <miangelse45@gmail.com>
2025-07-18 16:38:27 -0500

    🐛 Corrijo ruta del endpoint /pedido

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./App.css";
-import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import CancelarCita from './CancelarCita';
